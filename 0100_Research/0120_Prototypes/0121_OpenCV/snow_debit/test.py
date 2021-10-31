0f85c245e1288f3a30b70d4b98901811d8f017fb
Francelet Samy <samy.francelet@ik.me>
2021-10-31 09:30:53 +0100

    Update test.py

--- 0100_Research/0120_Prototypes/0121_OpenCV/snow_debit/test.py
+++ 0100_Research/0120_Prototypes/0121_OpenCV/snow_debit/test.py
@@ -1,6 +1,16 @@
 import csv
 
-with open('eggs.csv', 'w', newline='') as csvfile:
-    spamwriter = csv.writer(csvfile)
-    spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
-    spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
+def csvTest():
+    with open('eggs.csv', 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile)
+        spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
+        spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
+
+def main():
+    l = [('Result_1', 'Result_2', 'Result_3', 'Result_4'), (1, 2, 3, 4), (5, 6, 7, 8)]
+    cs = zip(*l)
+    print(l)
+    print(cs)
+
+if __name__ == "__main__":
+    main()
